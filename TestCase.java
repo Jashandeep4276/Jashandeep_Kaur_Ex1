5ccbaf7eb2a447372e79118bb6dc3039d3d1c287
JASHANDEEP <JASHANDEEP@DESKTOP-BJN2AL6.hitronhub.home>
2020-10-04 16:53:12 -0400

    Initial Commit

---
+++ TestCase.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.pkgcase;
+
+/**
+ *
+ * @author JASHANDEEP
+ */
+public class TestCase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int x = 1; // x represents an int value
+    int[] y = new int[10]; // y represents an array of int 
+m(x, y); // Invoke m with arguments x and y
+    System.out.println("x is " + x);
+    System.out.println("y[0] is " + y[0]);
+  }
+  public static void m(int number, int[] numbers) {
+    number = 1001; // Assign a new value to number
+    numbers[0] = 5555; // Assign a new value to numbers[0]
+  }
+}
